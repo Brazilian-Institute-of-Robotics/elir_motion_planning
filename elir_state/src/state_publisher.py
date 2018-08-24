efdb5922c5db3378ca169d94df1efb022f027938
Cleber Couto Filho <clebercoutof@gmail.com>
2018-08-24 18:21:12 -0300

    add the robot_states publisher

--- elir_state/src/state_publisher.py
+++ elir_state/src/state_publisher.py
@@ -7,16 +7,23 @@ from math import pi
 import time
 
 rospy.init_node('State_Publisher')
+register_to_rad = (360.0/4095)*(pi/180)
 
-# Name of the joints in the robotic arm
-# Name of motor ID [1,2,3,4,5,6] is specified in the list.
-joints = ["shoulder_pan_joint","shoulder_pitch_joint","shoulder_yaw_joint","elbow_pitch_joint","elbow_roll_joint","wrist_pitch_joint"]
+#Dictionary containing the joint name and master servo ID
+joints = {11:'joint1_f',12:'joint2_f',13:'joint1_b',14:'joint2_b',
+31:'joint_garra_tracao_f1',41:'joint_garra_tracao_f2',131:'joint_garra_tracao_b1',
+141:'joint_garra_tracao_b2',71:'joint_garra_tracao_ap',51:'joint_eixo_tracao_f1',
+61:'joint_eixo_tracao_f2',151:'joint_eixo_tracao_b1',161:'joint_eixo_tracao_b2',
+81:'joint_eixo_tracao_ap'
+}
 
-#Dynamixel Motors will not be in Position 0 when switched ON. Dynamixel motors will have some initial position value. 
-#In the URDF it is assumed that initial joint value is 0 radian. 
-#Hence offset is required to match the URDF and the real robot.
-#Offset for motor id [1,2,3,4,5,6] is specified in the list.
-offset = [666,512,512,375,512,682]
+#Offset of the joints, configured in the controllers config files
+offset = {'joint1_f':2380,'joint2_f':2791,'joint1_b':0,'joint2_b':0,
+'joint_garra_tracao_f1':0,'joint_garra_tracao_f2':0,'joint_garra_tracao_b1':0,
+ 'joint_garra_tracao_b2':0, 'joint_garra_tracao_ap':0,'joint_eixo_tracao_f1':0,
+'joint_eixo_tracao_f2':0,'joint_eixo_tracao_b1':0,'joint_eixo_tracao_b2':0,
+'joint_eixo_tracao_ap':0
+}
 
 '''
 Function: process(). Callback for subscriber of raw data from dynamixel motor. 
@@ -28,16 +35,27 @@ def process(msg):
 	joint_states = JointState()
 
 	joint_states.header.stamp = rospy.Time.now()
+	#Creates fake values for joints
+	for i in joints:
+		if not (i==11 or i ==12):
+			joint_states.name.append(joints[i])
+			joint_states.position.append(0)
+			joint_states.velocity.append(0)
 
-	for x in msg.motor_states:
-		joint_states.name.append(joints[x.id-1])
-
-		if(not (x.id==2 or x.id==4)):
-			joint_states.position.append((x.position-offset[x.id-1])*(300.0/1023)*(pi/180))
-		else:
-			joint_states.position.append((offset[x.id-1]-x.position)*(300.0/1023)*(pi/180))
-		#joint_states.velocity.append(x.velocity)
-	
+	for servo in msg.motor_states:
+		#Ignore slave servos
+		if servo.id in joints:
+			joint_name = joints[servo.id]
+			joint_offset = offset[joint_name]
+			joint_position = (servo.position - joint_offset)*register_to_rad
+			joint_velocity = servo.speed
+			print(joints[servo.id])
+			print("Position",servo.position)
+			print("Offset:",joint_offset)
+			print("Position moveit",joint_position)
+			joint_states.name.append(joint_name)
+			joint_states.position.append(joint_position)
+			joint_states.velocity.append(joint_velocity)
 	pub.publish(joint_states)
 
 # Subscriber for raw feedback from dynamixel motor. Position of the motor will be in the range of (0,1023).
