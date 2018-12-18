13914992e6e2d795994b51b29e4f7da1ce258a78
Cleber Couto Filho <clebercoutof@gmail.com>
2018-12-18 16:34:34 -0300

    Documentates de custom_move group

--- elir_move_group/src/move_group_python.py
+++ elir_move_group/src/move_group_python.py
@@ -1,39 +1,5 @@
 #!/usr/bin/env python
 
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2013, SRI International
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of SRI International nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Acorn Pooley, Mike Lautman
-
 import sys
 import copy
 import rospy
@@ -46,9 +12,8 @@ from moveit_commander.conversions import pose_to_list
 from elir_move_group.srv import MoveRequest
 from kinematics_solver.solver import elir_inverse_kinematics
 
-## END_SUB_TUTORIAL
-
 def all_close(goal, actual, tolerance):
+  #this function is based on the move it move group tutorials
   """
   Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
   @param: goal       A list of floats, a Pose or a PoseStamped
@@ -71,34 +36,39 @@ def all_close(goal, actual, tolerance):
   return True
 
 class MoveGroupElir(object):
-  """MoveGroupPythonIntefaceTutorial"""
+  """This class is used to create the ElirMoveGroup.
+  It contains the service wich receives the X and Z coordinates, the arm, and configuration, moving
+  the desired arm to the coordinates."""
   def __init__(self):
     super(MoveGroupElir, self).__init__()
     self.move_request_service = rospy.Service('motion_planning/MoveRequest',MoveRequest, self.go_to_pose_goal)
+    
     ## Instantiate a `RobotCommander`_ object. This object is the outer-level interface to
     ## the robot:
     robot = moveit_commander.RobotCommander()
-
+    #planning scene
     scene = moveit_commander.PlanningSceneInterface()
 
+    #Default arm configuration
     self.group_name = "f_arm"
     self.group = moveit_commander.MoveGroupCommander(self.group_name)
 
+    #Trajectory publisher
     display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                                    moveit_msgs.msg.DisplayTrajectory,
                                                    queue_size=20)
+    #Planner from elir_inverse_kinematics
     self.custom_planner = elir_inverse_kinematics()
 
     # Misc variables
     self.robot = robot
     self.scene = scene
-    #self.group = group
-    #self.planning_frame = planning_frame
-    #self.eef_link = eef_link
-    #self.group_names = group_names
 
   def go_to_joint_state(self,angle_vector):
-
+      """ Goes to the desired joint
+        :param float angle_vector: Angle containing joint1 and joint2 values
+        :return: 
+        --``all_close`` Bool result that compares the goal and the final value using the tolerance set"""
     theta1 = angle_vector[0]
     theta2 = angle_vector[1]
     joint_goal = self.move_group.get_current_joint_values()
@@ -116,26 +86,30 @@ class MoveGroupElir(object):
     return all_close(joint_goal, current_joints, 0.1)
 
   def go_to_pose_goal(self,req):
+      """ Service callback wich send the group to the 
+        :param MoveRequest req: Service Request variable
+        :return: 
+        --``result`` Bool result that compares the goal and the final value using the tolerance set"""
+    #Services variables containing the x and z goal, with the specified arm configuratino
     x_goal = req.x
     z_goal = req.z
     elbow_up = req.elbow_up
     
+    #Instantiates the group to be controlled
     self.group_name = req.group
     self.move_group = moveit_commander.MoveGroupCommander(self.group_name)
-
+    #Calculates the inverse kinematics from the elir_inverse_kinematics
     joint_target = self.custom_planner.inverse_kinematics(x_goal,z_goal,elbow_up)
-
+    #Goes to the joint state
     result = self.go_to_joint_state(joint_target)
-
     return result
 
 
 def main():
-  
+  #Main Function that initializes the node
   moveit_commander.roscpp_initialize(sys.argv)
-
   rospy.init_node('elir_move_group', anonymous=True)
-  x =MoveGroupElir()
-  rospy.spin()
+  
+  x = MoveGroupElir()
 if __name__ == '__main__':
   main()
